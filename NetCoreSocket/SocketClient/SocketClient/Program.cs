a7398409b26929140faed6bfbac00865ee13f80a
lxd <lxd491679063@163.com>
2021-04-15 18:15:16 +0800

    net_core demo

---
+++ NetCoreSocket/SocketClient/SocketClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NetData;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            NetDataReader _net_data_reader;
+        }
+    }
+}
---
+++ NetCoreSocket/SocketServer/NetData/NetData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace NetData
+{
+    public class NetDataReader
+    {
+        #region Properties
+
+        /// <summary>
+        /// m_stream 数据流
+        /// </summary>
+        MemoryStream m_stream = null;
+
+        /// <summary>
+        /// m_reader 二进制数据读取
+        /// </summary>
+        BinaryReader m_reader = null;
+
+        /// <summary>
+        /// m_dataLength 数据长度
+        /// </summary>
+        ushort m_dataLength;
+
+        #endregion
+
+        #region Ctors
+        public NetDataReader(byte[] data){
+            if (data != null)
+            {
+                m_stream = new MemoryStream(data);
+                m_reader = new BinaryReader(m_stream);
+
+                //m_dataLength = ReadUShort();
+            }
+        }
+        #endregion
+    }
+}
